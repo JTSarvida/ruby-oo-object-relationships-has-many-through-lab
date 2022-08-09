3ad489942fa99e17819bc1e2c886d5c22c0f482a
jtsarvida <jtsarvida@gmail.com>
2022-08-09 12:37:59 -0400

    commit tree 06664306ae17204fff3c35ec0db2911a300a990f

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,21 @@
+class Song
 
+    attr_accessor :name, :artist, :genre
+
+    @@all = [] 
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def self.songs
+        Song.all.select {|song| song.artist == self}
+    end
+end
